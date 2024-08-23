bc0b8701f08438ef6ed2f85ded528b2cea094366
Yihang Wang <wangyihanger@gmail.com>
2024-08-23 19:29:36 +0800

    feat: support download s3 object first

--- go.mod
+++ go.mod
@@ -2,14 +2,16 @@ module github.com/WangYihang/uio
 
 go 1.22.6
 
-require github.com/minio/minio-go/v7 v7.0.75
+require (
+	github.com/google/uuid v1.6.0
+	github.com/minio/minio-go/v7 v7.0.75
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
--- pkg/provider/s3.go
+++ pkg/provider/s3.go
@@ -2,18 +2,22 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 // ExtractS3Params extracts S3 parameters from the URL query.
-func extractS3Params(query url.Values) (endpoint, accessKey, secretKey string, insecure bool) {
-	return query.Get("endpoint"), query.Get("access_key"), query.Get("secret_key"), query.Get("insecure") == "true"
+func extractS3Params(query url.Values) (endpoint, accessKey, secretKey string, insecure bool, download bool) {
+	return query.Get("endpoint"), query.Get("access_key"), query.Get("secret_key"), query.Get("insecure") != "false", query.Get("download") == "true"
 }
 
 // logS3Params logs the S3 connection parameters.
@@ -28,7 +32,7 @@ func logS3Params(logger *slog.Logger, endpoint, bucketName, objectName string, i
 
 func OpenS3(uri *url.URL, logger *slog.Logger) (io.ReadCloser, error) {
 	query := uri.Query()
-	endpoint, accessKey, secretKey, insecure := extractS3Params(query)
+	endpoint, accessKey, secretKey, insecure, download := extractS3Params(query)
 	bucketName := uri.Host
 	objectName := strings.TrimLeft(uri.Path, "/")
 
@@ -43,16 +47,40 @@ func OpenS3(uri *url.URL, logger *slog.Logger) (io.ReadCloser, error) {
 		return nil, err
 	}
 
-	object, err := minioClient.GetObject(
-		context.Background(),
-		bucketName,
-		objectName,
-		minio.GetObjectOptions{},
-	)
-	if err != nil {
-		logger.Error("Failed to get S3 object", slog.String("error", err.Error()))
-		return nil, err
-	}
+	if download {
+		// Download to a temporary file
+		path := filepath.Join(os.TempDir(), uuid.New().String())
+		err := minioClient.FGetObject(
+			context.Background(),
+			bucketName,
+			objectName,
+			"",
+			minio.GetObjectOptions{},
+		)
+		if err != nil {
+			logger.Error("Failed to download S3 object", slog.String("error", err.Error()))
+			return nil, err
+		}
+		source := fmt.Sprintf("file://%s", path)
+		logger.Info("Downloaded S3 object", slog.String("url", source), slog.String("path", path))
+		uri, err := url.Parse(source)
+		if err != nil {
+			logger.Error("Failed to parse URL", slog.String("error", err.Error()))
+			return nil, err
+		}
+		return OpenFile(uri, logger)
+	} else {
+		object, err := minioClient.GetObject(
+			context.Background(),
+			bucketName,
+			objectName,
+			minio.GetObjectOptions{},
+		)
+		if err != nil {
+			logger.Error("Failed to get S3 object", slog.String("error", err.Error()))
+			return nil, err
+		}
 
-	return object, nil
+		return object, nil
+	}
 }
